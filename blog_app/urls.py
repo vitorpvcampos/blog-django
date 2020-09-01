e8dd0a2cf58446e4818b58204265ff379a6e6fe8
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-09-01 15:13:11 -0300

    List and detail views implemented close #9

---
+++ blog_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from blog_app import views
+
+app_name = 'blog_app'
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail,
+         name='post_detail'),
+]
