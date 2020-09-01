7bdf4bdf151e12413f3fdda982e532f61aff25cd
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-09-01 15:13:11 -0300

    List and detail views implemented close #9

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -36,3 +37,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog_app:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
