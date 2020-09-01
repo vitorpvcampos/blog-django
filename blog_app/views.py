e11d02bcb622190ec6db94e17ecd7b3c0a7354b9
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-09-01 20:38:14 -0300

    Added pagination close #13

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,13 +1,23 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 
 from blog_app.models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog_app/post/list.html',
-                  {'posts': posts})
+                  {'page': page,
+                   'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
