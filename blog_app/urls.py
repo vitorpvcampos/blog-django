16aa0af1708253e370c9b96a0250ee6a5e529301
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-09-01 20:53:27 -0300

    View changed to be class based close #15

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -5,7 +5,8 @@ from blog_app import views
 app_name = 'blog_app'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail,
          name='post_detail'),
