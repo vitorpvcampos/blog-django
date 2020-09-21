2b099ac3a07df254d91aeb936740035850ea8e69
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-09-21 15:21:38 -0300

    Created forms and sending emails structures close #19

--- blog_app/views.py
+++ blog_app/views.py
@@ -2,7 +2,10 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
+from blog.settings import EMAIL_HOST
 from blog_app.models import Post
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 def post_list(request):
@@ -37,3 +40,24 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog_app/post/list.html'
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, EMAIL_HOST,
+                      [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog_app/post/share.html', {'post': post, 'form': form, 'sent': sent})
