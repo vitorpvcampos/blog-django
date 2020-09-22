ba1092b2990c6556d9da21b0816302ef2a1642ce
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-09-22 18:33:20 -0300

    Added tagging functionality close #23

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,8 @@
+from django.db.models import Count
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from taggit.models import Tag
 
 from blog.settings import EMAIL_HOST
 from blog_app.models import Post
@@ -8,8 +10,12 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     try:
@@ -21,7 +27,8 @@ def post_list(request):
     return render(request,
                   'blog_app/post/list.html',
                   {'page': page,
-                   'posts': posts})
+                   'posts': posts,
+                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -40,12 +47,18 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+        .order_by('-same_tags', '-publish')[:4]
     return render(request,
                   'blog_app/post/detail.html',
                   {'post': post,
                    'comments': comments,
                    'new_comment': new_comment,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
