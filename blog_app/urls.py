2354c4313f2e71a6403024606c18b9c425b838ec
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-09-25 14:16:30 -0300

    Feed functionality added close #31

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from blog_app import views
+from blog_app.feeds import LatestPostsFeed
 
 app_name = 'blog_app'
 
@@ -11,4 +12,5 @@ urlpatterns = [
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('tag/<slug:tag_slug>/',
          views.post_list, name='post_list_by_tag'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
