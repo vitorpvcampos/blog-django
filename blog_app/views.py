954e320bc53cd8b22fe25846ba59c7dfe7e372ab
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-09-28 10:45:20 -0300

    Full-text search functionality added close #33

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.search import SearchVector
 from django.db.models import Count
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
@@ -6,7 +7,7 @@ from taggit.models import Tag
 
 from blog.settings import EMAIL_HOST
 from blog_app.models import Post
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 
 
@@ -87,3 +88,19 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog_app/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(search=SearchVector('title', 'body'), ).filter(search=query)
+    return render(request,
+                  'blog_app/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
